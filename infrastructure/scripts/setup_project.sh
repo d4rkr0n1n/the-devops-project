7331427bc14562cd798524f3834a23cca24c20d1
Mridul Roy <midlry.mr@gmail.com>
2024-05-13 17:20:02 +0530

    update 2 files, delete 1 file and create 1 file

--- infrastructure/scripts/setup_project.sh
+++ infrastructure/scripts/setup_project.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-sudo apt install python3-pip apache2 -y
+sudo apt-get update
+sudo apt-get install python3-pip apache2 -y
+
 sudo pip install bitmath
 sudo pip install psutil
 
 git clone https://github.com/d4rkr0n1n/the-devops-project.git
 
-sudo cp ~/the-devops-project/web/index_tmp.html /var/www/html/
-sudo cp ~/the-devops-project/web/create_or_update_webpage.py /var/www/html/
+sudo cp the-devops-project/web/index_tmp.html /var/www/html/
+sudo cp the-devops-project/web/styles.css /var/www/html/
+sudo cp the-devops-project/web/create_or_update_webpage.py /var/www/html/
 
+sudo systemctl enable --now apache2
+sudo python3 create_or_update_webpage.py 
--- infrastructure/scripts/upgrades.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sudo apt-get update --quiet
-sudo apt-get upgrade --with-new-pkgs --quiet -y
