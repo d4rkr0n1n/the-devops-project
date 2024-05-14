9d33be0f6b7b664690172dc3c1ea5f056193fd54
Mridul Roy <midlry.mr@gmail.com>
2024-05-14 15:34:17 +0530

    update setup_project.sh

--- infrastructure/scripts/setup_project.sh
+++ infrastructure/scripts/setup_project.sh
@@ -4,6 +4,8 @@ rm -rf the-devops-project
 
 git clone https://github.com/d4rkr0n1n/the-devops-project.git
 
+cd /var/www/html
+
 sudo cp -rf /home/vagrant/the-devops-project/web/* .
 
 sudo python3 create_or_update_webpage.py &
